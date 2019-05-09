00c524110a9a6cba15e6e85435692b2d2753fe60
0xnoob <49793844+0xnoob@users.noreply.github.com>
2019-05-09 21:10:43 +0200

    Added Codesandbox instruction to placeholder index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const Message = (
-  <p>
-    You need to remove this placeholder <mark>index.js</mark> file and rename
-    one of the <mark>index.*.js</mark> files to <mark>index.js</mark> in order
-    to play that version of <em>tic-tac-toe.</em>
-  </p>
+  <>
+    <h1>Tic-Tac-Toe with hooks & reducers</h1>
+    <h2>In CodeSandbox</h2>
+    <p>
+      Select an <code>index.*.js</code> file in the directory tree to your left
+      and toggle the <em>Project View</em> button in the upper right corner to
+      play that variant.
+    </p>
+    <h2>Otherwise</h2>
+    <p>
+      You need to remove or rename this placeholder <em>index.js</em> file and
+      rename one of the <em>index.*.js</em> files to <em>index.js</em> in order
+      to play that variant.
+    </p>
+  </>
 );
 ReactDOM.render(Message, document.getElementById("root"));
